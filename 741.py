d25510813f97b7f6106367ed7043cb80a76b7ef3
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-23 20:44:58 +0800

    Update 741.py

--- 741.py
+++ 741.py
@@ -6,16 +6,16 @@ def paly():
   while 1:
     b=int(input("請輸入%d到%d之間的整數:"%(start,end)))
     if b==a:
-      print("恭喜你中獎了")
+      st.write("恭喜你中獎了")
       break
     elif b>a:
       if b>=end:
-        print("輸入不合法,請重新輸入:")
+        st.write("輸入不合法,請重新輸入:")
       else:
         end=b
     else:
       if b<=start:
-        print("輸入不合法,請重新輸入:")
+        st.write("輸入不合法,請重新輸入:")
       else:
           start=b
 if __name__=='__main__':
